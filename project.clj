ba0792a910ca9ccd22f65238f774b9d66e66815a
hexagonrecursion <hexagonrecursion@gmail.com>
2020-10-10 09:12:20 +0000

    Update the dependencies

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
 
   :url "http://rigsomelight.com/2014/05/01/interactive-programming-flappy-bird-clojurescript.html"
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.312"]
-                 [org.clojure/core.async "0.4.474"]
-                 [cljsjs/react-dom-server "15.3.1-0"]  ;; for sablono
-                 [cljsjs/react-dom "15.3.1-0"] ;; for sablono
-                 [cljsjs/react "15.3.1-0"] ;; for sablono
-                 [sablono "0.7.5"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773"]
+                 [org.clojure/core.async "1.3.610"]
+                 [cljsjs/react-dom-server "16.13.1-0"]  ;; for sablono
+                 [cljsjs/react-dom "16.13.1-0"] ;; for sablono
+                 [cljsjs/react "16.13.1-0"] ;; for sablono
+                 [sablono "0.8.6"]]
 
   :resource-paths ["resources" "target"]
   
   :clean-targets ^{:protect false} ["target/public"]
   
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.5"]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.11"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
 
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
